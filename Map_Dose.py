eb6d6f236f650635e9fcc980bd43cc962036f8dd
Ane Vigre Håland <anevh@stud.ntnu.no>
2020-02-04 14:53:36 +0100

    updatet Map_Dose.py

--- Map_Dose.py
+++ Map_Dose.py
@@ -20,7 +20,7 @@ def pixelValueToDose(pixelValue):
 def mapDose(img_path):
     dataset = pydicom.dcmread(img_path)
     ds=dataset.pixel_array
-    rows, cols = np.shape(ds)
+    rows, cols = np.shape(ds) #må kanskje ve
     #rows= int(dataset.Rows)
     #cols = int(dataset.Columns)
     #ds= np.pixel_array(ds.PixelData)
@@ -29,3 +29,19 @@ def mapDose(img_path):
         for j in range(cols):
             dose_value[i,j]=pixelValueToDose(ds[i,j])
     return dose_value #er et bilde med dose-nivåer, med samme dimensjoner som PV-bildet
+
+def compare(img_path2):
+    film=mapDose(img_path)
+    dosePlanSys=pydicom.dcmread(img_path2)
+    ds=dosePlanSys.pixel_array
+    rows, cols = np.shape(ds)
+    compare_gamma=np.zeros((rows,cols))
+    tolerance=5 #ingen ide hva som er akseptabelt avvik her?
+    
+    for i in range(rows):
+        for j in range(cols):
+            if (abs(film-dosePlanSys)<tolerance):
+                compare_gamma[i,j]=2^16-1 #sett til hvit
+            else:
+                compare_gamma[i,j]=0 #sett til svart
+    return compare_gamma
